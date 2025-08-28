80d6963c060cb868923abadb3ce848c312fd7d54
Javier Eusamio <jaymaeuro@gmail.com>
2025-08-28 12:40:41 +0200

    Refactor Family and Genus models
    Removed unused fields from Family and Genus data classes:
    - `nGenera`, `nSpecies`, and `genera` from `Family`
    - `nSpecies` and `species` from `Genus`

--- app/src/main/java/com/floracatalana/floracatalana/domain/model/Family.kt
+++ app/src/main/java/com/floracatalana/floracatalana/domain/model/Family.kt
@@ -6,12 +6,9 @@ import androidx.annotation.Keep
 @Keep
 data class Family(
     val code: String = "",
-    val nGenera: Int = 0,
     val nameCat: String = "",
     val nameLatin: String = "",
     val nodeId: String = "",
     val rank: String = "",
     val url: String = "",
-    val nSpecies: Int = 0,
-    val genera: List<ShortTaxon> = listOf()
 )
--- app/src/main/java/com/floracatalana/floracatalana/domain/model/Genus.kt
+++ app/src/main/java/com/floracatalana/floracatalana/domain/model/Genus.kt
@@ -7,11 +7,9 @@ import androidx.annotation.Keep
 data class Genus(
     val code: String = "",
     val shortFamily: ShortTaxon = ShortTaxon(),
-    val nSpecies: Int = 0,
     val nameCat: String = "",
     val nameLatin: String = "",
     val nodeId: String = "",
     val rank: String = "",
     val url: String = "",
-    val species: List<ShortTaxon> = listOf()
 )
