b9895e012d51a79c44c925ac7ac227e48eeb58da
Javier Eusamio <jaymaeuro@gmail.com>
2025-08-28 13:08:04 +0200

    Map `field_fam` to `shortFamily` in `GenusDetailMapper`

--- app/src/main/java/com/floracatalana/floracatalana/domain/mappers/GenusDetailMapper.kt
+++ app/src/main/java/com/floracatalana/floracatalana/domain/mappers/GenusDetailMapper.kt
@@ -3,6 +3,7 @@ package com.floracatalana.floracatalana.domain.mappers
 import com.floracatalana.floracatalana.data.remote.HttpRoutes
 import com.floracatalana.floracatalana.data.remote.dto.GenusDetailResponse
 import com.floracatalana.floracatalana.domain.model.Genus
+import com.floracatalana.floracatalana.domain.model.ShortTaxon
 
 fun GenusDetailResponse.toGenus(): Genus {
     val nodeId = (nid.firstOrNull()?.value ?: 0).toString()
@@ -13,9 +14,11 @@ fun GenusDetailResponse.toGenus(): Genus {
         nameLatin = field_nom_del_genere1.firstOrNull()?.value ?: "",
         nodeId = nodeId,
         url = url,
+        shortFamily = field_fam.firstOrNull()?.let {
+            ShortTaxon(
+                code = it.target_id.toString(),
+            )
+        } ?: ShortTaxon(),
 //        nameCat = TODO(),
-//        shortFamily = TODO(),
-//        nSpecies = TODO(),
-//        species = TODO()
     )
 }
